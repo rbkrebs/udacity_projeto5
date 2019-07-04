6500c1c56d112996f310b265f729edc3414beb5a
romulo <rbkrebs>
2019-07-04 19:25:45 -0300

    add more places on database

--- list_markers.py
+++ list_markers.py
@@ -16,7 +16,10 @@ session = DBSession()
 listMarkers = [{'title': 'Legislative Assembly of Rio Grande do Sul', 'lat': '-30.0328', 'infoWindow': 'I really do not like this place!!!', 'lng': '-51.2311', 'favorite': 'False'},
                {'title': 'Marinha do Brasil Park', 'lat': '-30.056944444444', 'infoWindow': 'I have never gone to this places, even living so near',
                    'lng': '-51.232222222222', 'favorite': 'False'},
-               {'title': 'Estádio dos Eucaliptos', 'lat': '-30.061666666667', 'infoWindow': 'I do not know anything about this place', 'lng': '-51.227222222222', 'favorite': 'False'}]
+               {'title': 'Estádio dos Eucaliptos', 'lat': '-30.061666666667', 'infoWindow': 'I do not know anything about this place', 'lng': '-51.227222222222', 'favorite': 'False'},
+               {'title': 'Federal University of Health Sciences of Porto Alegre', 'lat': '-30.031576', 'infoWindow': 'I have used to work here for 5 long years!!', 'lng': '-51.220472', 'favorite': 'True'},
+               {'title': 'Farroupilha Park', 'lat': '-30.036666666667', 'infoWindow': 'A very good park to practise exercise, but too dangers at night!!', 'lng': '-51.215833333333', 'favorite': 'True'},
+               {'title': 'Santa Casa de Misericordia Hospital', 'lat': '-30.0305', 'infoWindow': 'A very important hospital.', 'lng': '-51.2222', 'favorite': 'True'}]
 
 for item in listMarkers:
 
